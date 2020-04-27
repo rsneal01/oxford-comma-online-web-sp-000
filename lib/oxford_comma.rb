68b1bfec6f75e3ec416a024e6f5eae9d569b455a
Bobby Neal <rsneal01@gmail.com>
2020-04-27 23:43:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join(",") 
+      # formats no additinal formating when 1 element array
+  elsif array.length == 2
+    array.join(" and ")
+      # formats (a and b) when 2 element array
+  elsif array.length > 2 
+    array.last.insert(0, "and ")
+    array.join(", ")
+      # formats (a, b, and c) when 3 element array or longer
+  
 end
+end
+
+
